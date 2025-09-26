8b2d1fbd1ac088a12ed22cca7e535c08dca120be
Admin <admin@data.syndicat-reyssouze.fr>
2025-09-26 15:00:36 +0200

    Regenerate requirements Correct dependancies

--- pages/2_Rapport.py
+++ pages/2_Rapport.py
@@ -38,11 +38,11 @@ from dateutil.relativedelta import *
 
 #from PIL import Image
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+#from selenium import webdriver
+#from selenium.webdriver.chrome.service import Service as ChromeService
+#from webdriver_manager.chrome import ChromeDriverManager
 
-from selenium.webdriver.chrome.options import Options  
+#from selenium.webdriver.chrome.options import Options  
 
 
 ################
