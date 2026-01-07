81ae895df4581fb119e2e72942ce5b3099c1ead7
SBVR-Jeremy <jeremy.bluteau@syndicat-reyssouze.fr>
2026-01-07 12:13:24 +0100

    Remove dep

--- pages/4_Rapport2.py
+++ pages/4_Rapport2.py
@@ -90,7 +90,7 @@ from tools.queries import *
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options  
 
 # ---------------------------------------------------------------------------------------------
